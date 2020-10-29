0181a8863e0e9e005f3ceac5ea57667b85b2272a
Márcio Lôbo <cast.mlobo.utic@sebrae.com.br>
2020-10-29 05:34:18 -0300

    roda-pé

---
+++ web/src/components/Footer/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width:100%;
+    height: 70px;
+    background-color: #20295F;
+    position:fixed;
+    bottom:0;
+    display: flex;
+    border-bottom: 5px solid #EE6B26;
+    align-items:center;
+    justify-content:center;
+
+    span{
+        color:#FFF;
+        font-weight:bold;
+    }
+
+ 
+
+`
